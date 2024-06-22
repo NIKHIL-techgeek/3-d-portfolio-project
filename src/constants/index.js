8cdea03a1531d539e446fd66200a86155677ad4b
Nikhil Kapoor <83280734+NIKHIL-techgeek@users.noreply.github.com>
2024-06-22 15:46:56 -0400

    working on tech page

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,11 +116,11 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
+      title: "IT Analyst",
+      company_name: "Entegrus",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "Jan 2024 - April 2024",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -129,11 +129,11 @@ import {
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
+      title: "Teaching Assistant",
+      company_name: "University of Windsor",
       icon: tesla,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Jan 2023 - December 2023",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -142,7 +142,7 @@ import {
       ],
     },
     {
-      title: "Web Developer",
+      title: "Data Entry Clerk",
       company_name: "Shopify",
       icon: shopify,
       iconBg: "#383E56",
