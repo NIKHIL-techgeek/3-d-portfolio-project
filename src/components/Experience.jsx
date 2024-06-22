f12a0b502bd75dcb2e0d4179ec0ca745a4320b4e
Nikhil Kapoor <83280734+NIKHIL-techgeek@users.noreply.github.com>
2024-06-22 14:49:49 -0400

    working on experience section

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+const Experience = () => {
+  return (
+    <div>Experience</div>
+  )
+}
+
+export default Experience
